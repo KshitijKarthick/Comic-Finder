411c3cd1cc4f0ff011f5066d349d33c13724aaa4
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-03-08 14:20:40 +0530

    Crawler Script

---
+++ crawler.py
@@ -0,0 +1,13 @@
+import server
+from server import Database
+import configparser
+
+if __name__ == '__main__':
+    ''' Script to run the Crawler '''
+
+    server_config = configparser.RawConfigParser()
+    server_config.read('server.conf')
+    database_url=server_config.get('Database', 'database_url')
+    database_port=server_config.get('Database', 'database_port')
+    database = Database(database_url,database_port)
+    database.get_data()
