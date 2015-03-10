a941d04a6b8eed1794d5e6598c2ef5d1a82a2a20
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-03-10 20:53:43 +0530

    Refactoring, Database Code seperated from Server

---
+++ database.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+import re
+from bson import Regex
+from pymongo import MongoClient
+class Database():
+    '''
+        Database Configuration:
+        client -> MongoDb client.
+        db -> Retrieve the Database.
+        collection -> Retrieve the Collection.
+    '''
+
+    client = ""
+    db = ""
+    collection = ""
+
+    def __init__(self, database_uri ):
+        ''' Initialization the URL of the comic and transcript '''
+
+        try:
+            self.client = MongoClient(database_uri)
+            self.db = self.client.get_default_database()
+            self.collection = self.db.xkcd
+        except:
+            print("Error Could not Connect with the Database\nMake sure connection can be estabilished with the Database.")
+            exit(-1)
+
+    def insert_data(self, data):
+        ''' Insert the data into the Collection '''
+
+        self.collection.insert(data)
+    def find_data(self, string):
+        ''' Find the string in the Database '''
+
+        pattern = re.compile(string, re.IGNORECASE)
+        regex = Regex.from_native(pattern)
+        regex.flags ^= re.UNICODE
+        return self.collection.find({"transcript":regex})
