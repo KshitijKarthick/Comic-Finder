23bc5e119725878d6e04380969d673de9e3af8c0
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-03-13 18:22:27 +0530

    Crawler stores previous data in Db, and resumes execution on start

--- crawler.py
+++ crawler.py
@@ -8,32 +8,44 @@ import re
 def crawl_data(database):
         ''' Obtain the data for storing into the Collection '''
 
-        errors=[]
-        link_id=0
+        errors = []
+        try:
+            link_id = database.collection.find_one({'crawler':'crawler'})['id']
+        except TypeError:
+            link_id = 0
         while(True):
             link_id+=1
-            print(link_id)
-            opener=urllib.request.build_opener()
-            try:
-                webpage=opener.open("http://www.xkcd.com/"+str(link_id)+"/info.0.json")
-            except:
-                if link_id == 404:
-                    continue;
-                else:
-                    break;
-            if webpage.status == 200:
+            if database.collection.find_one({'id' : link_id}):
+                continue
+            else:
+                opener=urllib.request.build_opener()
                 try:
-                    comic_data = json.loads((webpage.readall()).decode("utf-8"))
-                    webpage.close()
-                    database.insert_data({
-                        'id':link_id,
-                        'transcript': re.sub("{{.*}}","",comic_data['transcript'],0),
-                        'title': comic_data['title'],
-                        'img': comic_data['img']
-                    })
+                    webpage=opener.open("http://www.xkcd.com/"+str(link_id)+"/info.0.json")
                 except:
-                    errors.append(link_id)
-        print(errors," -> Could not be Downloaded does not follow the standard format")
+                    if link_id == 404:
+                        continue;
+                    else:
+                        break;
+                if webpage.status == 200:
+                    print(link_id)
+                    try:
+                        comic_data = json.loads((webpage.readall()).decode("utf-8"))
+                        webpage.close()
+                        database.insert_data({
+                            'id':link_id,
+                            'transcript': re.sub("{{.*}}","",comic_data['transcript'],0),
+                            'title': comic_data['title'],
+                            'img': comic_data['img'],
+                            'rank':0
+                        })
+                    except:
+                        errors.append(link_id)
+        database.collection.update({'crawler':'crawler'},{"$set":{
+                'crawler':'crawler',
+                'id':link_id - 1
+            }},upsert=True)
+        if len(errors) > 0 :
+            print(errors," -> Could not be Downloaded does not follow the standard format")
         return errors
 
 if __name__ == '__main__':
